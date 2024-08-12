4be53fd9e550307ae379d8bc776891d4c630e440
michaelVortexg <michfrancogalardon@gmail.com>
2024-08-11 20:09:40 -0600

    Update videochat.js

--- videochat.js
+++ videochat.js
@@ -1,100 +1,46 @@
-var peer = new Peer()
-var myStream
-var peerList = []
-
-
-//this function will be initiating the peer
-function init(userId){
-  peer = new Peer(userId)
-  peer.on('open',(id)=>{
-    console.log(id+" connected") //if we connect successfully this will print
-  })
-
-  listenToCall()
-}
-
-
-//this function will keep listening to call or incoming events
-function listenToCall(){
-  peer.on('call',(call)=>{
-    navigator.mediaDevices.getUserMedia({
-      video:true,
-      audio: true
-    }).then((stream)=>{
-
-      myStream = stream
-      addLocalVideo(stream)
-      call.answer(stream)
-      call.on('stream',(remoteStream)=>{
-        if(!peerList.includes(call.peer)){
-          addRemoteVideo(remoteStream)
-          peerList.push(call.peer)
-        }
-      })
-    }).catch((err)=>{
-      console.log("unable to connect because "+err)
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then((stream) => {
+        // Use the stream for local video
+        document.getElementById('localVideo').srcObject = stream;
     })
-  })
-}
+    .catch((error) => {
+        console.error('Error accessing media devices:', error);
+    });
 
-//this function will be called when we try to make a call
-function makeCall(receiverId){
-    navigator.mediaDevices.getUserMedia({
-      video:true,
-      audio: true
-    }).then((stream)=>{
-      myStream = stream
-      addLocalVideo(stream)
-      let call = peer.call(receiverId,stream)
-      call.on('stream',(remoteStream)=>{
-        if(!peerList.includes(call.peer)){
-          addRemoteVideo(remoteStream)
-          peerList.push(call.peer)
-        }
-      })
-    }).catch((err)=>{
-      console.log("unable to connect because "+err)
+let peerConnection = new RTCPeerConnection();
+
+// Add local stream to the peer connection
+stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+// Create an offer
+peerConnection.createOffer()
+    .then((offer) => peerConnection.setLocalDescription(offer))
+    .then(() => {
+        // Send the offer to the other peer
     })
+    .catch((error) => console.error('Error creating offer:', error));
 
-}
+// On the other side, handle the offer and create an answer
+// Then set the remote description of the peer connection
 
-//this function will add local stream to video pannel
-function addLocalVideo(stream){
-  let video = document.createElement("video")
-  video.srcObject = stream
-  video.classList.add("video")
-  video.muted = true // local video need to be mute because of noise issue
-  video.play()
-  document.getElementById("localVideo").append(video)
-}
+let dataChannel = peerConnection.createDataChannel('myDataChannel');
 
-//this function will add remote stream to video pannel
-function addRemoteVideo(stream){
-  let video = document.createElement("video")
-  video.srcObject = stream
-  video.classList.add("video")
-  video.play()
-  document.getElementById("remoteVideo").append(video)
-}
+dataChannel.onopen = (event) => {
+    // Data channel is open
+};
 
+dataChannel.onmessage = (event) => {
+    // Handle received data
+};
 
+peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+        // Send the ICE candidate to the other peer
+    }
+};
 
-//so far video call is working fine.. lets toggle the video or audio
-function toggleVideo(b){
-  if(b=="true"){
-    myStream.getVideoTracks()[0].enabled = true
-  }
-  else{
-    myStream.getVideoTracks()[0].enabled = false
-  }
-}
+peerConnection.ontrack = (event) => {
+    document.getElementById('remoteVideo').srcObject = event.streams[0];
+};
+ Estos son sólo ejemplos básicos y las 
 
-//toggle audio
-function toggleAudio(b){
-  if(b=="true"){
-    myStream.getAudioTracks()[0].enabled = true
-  }
-  else{
-    myStream.getAudioTracks()[0].enabled = false
-  }
-}
